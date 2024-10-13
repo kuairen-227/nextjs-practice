54cec17fcabf4222cf4a2459c6ab204b6dfde131
kaito sasaki <kaito_127@icloud.com>
2024-10-13 03:33:25 +0000

    feat: :sparkles: Homeアイコンにリンク追加

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -13,7 +13,9 @@ export const Header = () => {
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center">
+        <Link href={"/"}>
         <IoHomeOutline width={100} height={100} />
+        </Link>
         <Link href={"/"}>ダッシュボード</Link>
         <NavProjects />
         <NavRecentView />
