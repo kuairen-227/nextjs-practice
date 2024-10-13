32194ff8c0dbbc9f6d2c1489ba73bc907a197bbc
kaito sasaki <kaito_127@icloud.com>
2024-10-13 03:37:00 +0000

    feat: :lipstick: 背景色追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-gray-50">
         <header>
           <Header />
         </header>
