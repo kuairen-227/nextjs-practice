57cb5dbca10e51fd667cd650dbafeb21b5071c3d
kaito sasaki <kaito_127@icloud.com>
2024-10-20 07:54:25 +0000

    fix: :bug: lang属性を日本語に変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className="bg-gray-100">
         <header>
           <Header />
